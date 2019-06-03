53eba13a60eff26bb48b05f848780a18e95fd1ef
Adam Hammond <adam@hammo.io>
2019-06-04 09:43:07 +1000

    Updated version to roll new PyPi package.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws-creds",
-    version="0.1.1",
+    version="0.1.2",
     author="Adam Hammond",
     author_email="adam@hammo.io",
     description="Switch between AWS Credential profiles.",

34d7d57bfc73ef4e72c8b8d7113165ad20585e35
Adam Hammond <adam@hammo.io>
2019-06-12 16:28:54 +1000

    Bumped version number to 0.0.1 to reflect bug fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws-creds",
-    version="1.0.0",
+    version="1.0.1",
     author="Adam Hammond",
     author_email="adam@hammo.io",
     description="Easily switch between AWS Credential profiles.",
